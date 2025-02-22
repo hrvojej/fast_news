69dd77d77f47b5aa05eae320952091c669acbb85
Hrvoje <hrvojej@gmail.com>
2025-02-22 02:32:19 +0100

    schedule dagster to run on every 2 hours from 1 hour

--- news_aggregator/dagster_orchestration/schedules.py
+++ news_aggregator/dagster_orchestration/schedules.py
@@ -11,7 +11,7 @@ from dagster_jobs import (
 )
 
 @schedule(
-    cron_schedule="0 * * * *",
+    cron_schedule="0 */2 * * *",  # Runs at minute 0 every 2 hours
     job=abc_news_job,
     execution_timezone="UTC",
     default_status=DefaultScheduleStatus.RUNNING,
@@ -20,7 +20,7 @@ def abc_news_schedule(_context):
     return {}
 
 @schedule(
-    cron_schedule="7 * * * *",
+    cron_schedule="7 */2 * * *",  # Runs at minute 7 every 2 hours
     job=aljazeera_news_job,
     execution_timezone="UTC",
     default_status=DefaultScheduleStatus.RUNNING,
@@ -29,7 +29,7 @@ def aljazeera_news_schedule(_context):
     return {}
 
 @schedule(
-    cron_schedule="14 * * * *",
+    cron_schedule="14 */2 * * *",  # Runs at minute 14 every 2 hours
     job=bbc_news_job,
     execution_timezone="UTC",
     default_status=DefaultScheduleStatus.RUNNING,
@@ -38,7 +38,7 @@ def bbc_news_schedule(_context):
     return {}
 
 @schedule(
-    cron_schedule="21 * * * *",
+    cron_schedule="21 */2 * * *",  # Runs at minute 21 every 2 hours
     job=fox_news_job,
     execution_timezone="UTC",
     default_status=DefaultScheduleStatus.RUNNING,
@@ -47,7 +47,7 @@ def fox_news_schedule(_context):
     return {}
 
 @schedule(
-    cron_schedule="28 * * * *",
+    cron_schedule="28 */2 * * *",  # Runs at minute 28 every 2 hours
     job=guardian_news_job,
     execution_timezone="UTC",
     default_status=DefaultScheduleStatus.RUNNING,
@@ -56,7 +56,7 @@ def guardian_news_schedule(_context):
     return {}
 
 @schedule(
-    cron_schedule="35 * * * *",
+    cron_schedule="35 */2 * * *",  # Runs at minute 35 every 2 hours
     job=nyt_news_job,
     execution_timezone="UTC",
     default_status=DefaultScheduleStatus.RUNNING,
@@ -65,7 +65,7 @@ def nyt_news_schedule(_context):
     return {}
 
 @schedule(
-    cron_schedule="42 * * * *",
+    cron_schedule="42 */2 * * *",  # Runs at minute 42 every 2 hours
     job=reuters_news_job,
     execution_timezone="UTC",
     default_status=DefaultScheduleStatus.RUNNING,
@@ -74,7 +74,7 @@ def reuters_news_schedule(_context):
     return {}
 
 @schedule(
-    cron_schedule="49 * * * *",
+    cron_schedule="49 */2 * * *",  # Runs at minute 49 every 2 hours
     job=cnn_news_job,
     execution_timezone="UTC",
     default_status=DefaultScheduleStatus.RUNNING,
